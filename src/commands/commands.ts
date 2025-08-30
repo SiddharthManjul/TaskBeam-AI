0d877e2e0c8ffde13100a7bf46a039681c29c2ca
SiddharthManjul <manjulsiddharth09@gmail.com>
2025-08-30 22:46:04 +0530

    Help Command

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -26,3 +26,28 @@ Try any of these commands to get started!
     await bot.sendMessage(chatId, welcomeMessage, { parse_mode: "Markdown" });
   },
 };
+
+export const helpCommand: BotCommand = {
+    command: "/help",
+    description: "Get list of available commands",
+    handler: async (bot: TelegramBot, msg: TelegramBot.Message) => {
+        const chatId = msg.chat.id;
+        const helpMessage = `
+        *Available Commands:*
+        
+        /start - Welcome message and bot introduction
+        /help - Show the help message
+        /echo <text> - Echo back your message
+        /time - Get current time
+        /joke - Get a random joke
+        /weather - Get weather information (demo)
+        /ping - Test bot responsiveness
+        
+        Just type any command to use it!
+        `;
+
+        await bot.sendMessage(chatId, helpMessage, {parse_mode: 'Markdown'});
+    },
+};
+
+
